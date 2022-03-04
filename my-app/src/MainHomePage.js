c428e3835c371b29ad49c708a87aebc33cf581f3
Pallavi <pallavilpmt1995@gmail.com>
2022-03-04 13:09:01 +0530

    Fixed Navbar str for small size screens(Responsize) using @media-query.

--- my-app/src/MainHomePage.js
+++ my-app/src/MainHomePage.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function MainHomePage(){
   return(
     <>
+    <h2>This is main content</h2>
     </>
   )
 }
--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -4,22 +4,17 @@ export default function Navbar(){
     return(
         <>
   <nav className="navbar background h-nav-resp">
+  <div className="logo"><img src="/juslogo1.png" alt="logo"/><span class="crms-text">CRMS</span></div>
+  <input type="checkbox" id="check" />
+  <label for="check" class="checkbtn">
+    <i class="fas fa-bars"></i>
+  </label>
   <ul className="nav-list v-class-resp">
-  <div className="logo"><img src="/juslogo1.png" alt="logo"/></div>
   <li><a href="index.html">Home</a></li>
   <li><a href="About.html">About</a></li>
   <li><a href="#services">Services</a></li>
   <li><a href="contact.html">Contact</a></li>
   </ul>
-  <div className="rightnav v-class-resp">
-  <input type="text" name="search" id="search" placeholder="Search" />
-  <button className="btn btn-sm" >Search</button>
-  </div>
-  <div className="burger">
-  <div className="line"></div>
-  <div className="line"></div>
-  <div className="line"></div>
-  </div>
         </nav>
         </>
     )
