d5ebb0c45ec6557c1f5d69bfbf95893f234749b6
Pallavi <pallavilpmt1995@gmail.com>
2022-04-13 12:29:38 +0530

    add FIR overview structure.

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -12,7 +12,7 @@ export default function Navbar(){
   <ul className="nav-list v-class-resp">
   <li><a href="/FIRform">Home</a></li>
   <li><a href="About.html">About</a></li>
-  <li><a href="#services">Services</a></li>
+  <li><a href="">Complaint Details</a></li>
   <li><a href="contact.html">Contact</a></li>
   </ul>
         </nav>
